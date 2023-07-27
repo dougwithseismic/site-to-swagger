6b7175983f754c90c898253ab4377670795f9db3
Doug Silkstone <doug@withseismic.com>
2023-07-27 16:47:57 +0300

    Refactor pt.1

--- src/utility/harToSwagger.ts
+++ src/utility/harToSwagger.ts
@@ -1,22 +1,230 @@
 import SwaggerParser from '@apidevtools/swagger-parser'
-
 import logger from '@/middleware/logger'
 import fs from 'fs'
 
+// Base JSON types
 type JsonValue = string | number | boolean | null | JsonObject | JsonArray
 type JsonObject = { [key: string]: JsonValue }
 type JsonArray = JsonValue[]
 
+// JSON Schema
 type JsonSchema = {
     type: 'string' | 'number' | 'integer' | 'boolean' | 'object' | 'array' | 'null'
     items?: JsonSchema
     properties?: Record<string, JsonSchema>
 }
 
-/**
- * Convert path segments matching certain patterns (like UUIDs or digits) into query parameters.
- * For example, /user/12345-5678/get becomes /user/:userId/get.
- */
+const UUID_REGEX = /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/
+
+// Split the logic of detecting the segment type from the main function.
+function detectSegmentType(segment: string): {
+    type: 'uuid' | 'integer' | 'float' | 'none'
+    value: string
+} {
+    if (UUID_REGEX.test(segment)) {
+        return { type: 'uuid', value: segment }
+    } else if (/^\d+$/.test(segment)) {
+        return { type: 'integer', value: segment }
+    } else if (/^\d+(\.\d+)?$/.test(segment)) {
+        return { type: 'float', value: segment }
+    }
+    return { type: 'none', value: segment }
+}
+
+function loadHarFile(filePath: string): any {
+    try {
+        const harContent = JSON.parse(fs.readFileSync(filePath, 'utf8'))
+        return harContent
+    } catch (error) {
+        logger.error(`Error parsing HAR file: ${error}`)
+    }
+}
+
+type Paths = {
+    [path: string]: {
+        [method: string]: {
+            parameters: any[]
+            requestBody?: any
+            responses: any
+            summary?: string
+            tags?: string[]
+        }
+    }
+}
+
+type ParameterLocation = 'path' | 'query' | 'header'
+
+type Method = 'get' | 'post' | 'put' | 'delete' | 'patch'
+
+type Entry = {
+    request: {
+        url: string
+        method: string
+        queryString?: Array<{ name: string; value: string }>
+        headers?: Array<{ name: string; value: string }>
+        postData?: {
+            mimeType: string
+            text: string
+        }
+    }
+    response: {
+        content: {
+            mimeType: string
+            text?: string
+        }
+        status: number
+    }
+}
+
+type HARContent = {
+    log: {
+        entries: Entry[]
+    }
+}
+
+async function parseHARtoSwagger(filePath: string): Promise<any> {
+    const harContent: HARContent = loadHarFile(filePath)
+    const { entries } = harContent.log
+
+    const paths: Paths = {}
+    const servers: Array<{ url: string }> = []
+
+    for (const entry of entries) {
+        const { request, response } = entry
+        const url = new URL(request.url)
+        let path = url.pathname
+
+        addServerIfNotExists(servers, `${url.protocol}//${url.host}`)
+
+        if (response.content.mimeType !== 'application/json') continue // Skip if not a JSON response.
+
+        addPathIfNotExists(paths, path)
+
+        const method = request.method.toLowerCase() as Method
+
+        const existingMethod = paths[path][method]
+        if (!existingMethod) {
+            paths[path][method] = {
+                parameters: [],
+                responses: {},
+                summary: url.href,
+            }
+        }
+        // path = convertToQueryParams(path, paths[path][method].parameters)
+
+        const queryParamPath = convertToQueryParams(path, paths[path][method].parameters) // Convert path segments to query parameters like /api/user/1234 to /api/user/:userId
+
+        path = swapOriginalPathForDynamic(queryParamPath, path, paths)
+
+        handleQueryParams(paths, path, method, request.queryString)
+        handleHeaders(paths, path, method, request.headers)
+        handleRequestBody(paths, path, method, request.postData)
+        handleResponseBody(paths, path, method, response)
+    }
+
+    assignTagsToPaths(paths)
+
+    const report = generateReport(paths)
+    logger.info(`API Report: ${JSON.stringify(report, null, 2)}`)
+
+    return createSwaggerDocs(paths, servers)
+}
+
+function addServerIfNotExists(servers: Array<{ url: string }>, url: string): void {
+    if (!servers.some((s) => s.url === url)) {
+        servers.push({ url })
+    }
+}
+
+function addPathIfNotExists(paths: Paths, path: string): void {
+    if (!paths[path]) {
+        paths[path] = {}
+    }
+}
+
+function swapOriginalPathForDynamic(queryParamPath: string, path: string, paths: Paths): string {
+    if (queryParamPath !== path) {
+        paths[queryParamPath] = paths[path]
+        delete paths[path]
+        return queryParamPath
+    }
+    return path
+}
+
+function handleQueryParams(
+    paths: Paths,
+    path: string,
+    method: Method,
+    queryParams?: Array<{ name: string; value: string }>
+): void {
+    if (!queryParams) return
+
+    for (const { name, value } of queryParams) {
+        paths[path][method].parameters.push({
+            name: name,
+            in: 'query' as ParameterLocation,
+            required: true,
+            example: value,
+            schema: { type: 'string' },
+        })
+    }
+}
+
+function handleHeaders(
+    paths: Paths,
+    path: string,
+    method: Method,
+    headers?: Array<{ name: string; value: string }>
+): void {
+    if (!headers) return
+
+    for (const { name, value } of headers) {
+        if (['host', 'content-length'].includes(name.toLowerCase())) continue
+
+        paths[path][method].parameters.push({
+            name: name,
+            in: 'header' as ParameterLocation,
+            required: true,
+            example: value,
+            schema: { type: 'string' },
+        })
+    }
+}
+
+function handleRequestBody(
+    paths: Paths,
+    path: string,
+    method: Method,
+    postData?: { mimeType: string; text: string }
+): void {
+    if (!postData) return
+
+    paths[path][method].requestBody = {
+        content: {
+            [postData.mimeType]: {
+                schema: { type: 'object' }, // You might need a more complex method to infer the schema.
+                example: postData.text,
+            },
+        },
+    }
+}
+
+function handleResponseBody(
+    paths: Paths,
+    path: string,
+    method: Method,
+    response: { content: { mimeType: string; text?: string }; status: number }
+): void {
+    const responseContent = {
+        content: {
+            [response.content.mimeType]: {
+                schema: { type: 'object' }, // Again, you might need a better way to infer the schema.
+                example: response.content.text,
+            },
+        },
+    }
+    paths[path][method].responses[response.status] = responseContent
+}
 
 function convertToQueryParams(path: string, existingParams: any[]): string {
     const segments = path.split('/')
@@ -62,184 +270,24 @@ function convertToQueryParams(path: string, existingParams: any[]): string {
 
     return segments.join('/')
 }
-
-function loadHarFile(filePath: string): any {
-    try {
-        const harContent = JSON.parse(fs.readFileSync(filePath, 'utf8'))
-        return harContent
-    } catch (error) {
-        logger.error(`Error parsing HAR file: ${error}`)
-    }
-}
-
-type Paths = {
-    [path: string]: {
-        [method: string]: {
-            parameters: any[]
-            requestBody?: any
-            responses: any
-            summary?: string
+function assignTagsToPaths(paths: Paths): void {
+    for (const path in paths) {
+        for (const method in paths[path]) {
+            const tags = extractTagsFromPath(path, paths)
+            paths[path][method].tags = tags
         }
     }
 }
 
-async function parseHARtoSwagger(filePath: string): Promise<any> {
-    const harContent = loadHarFile(filePath)
+function extractTagsFromPath(path: string, paths: Paths) {
+    const mostCommonSegment = getMostCommonSegment(paths)
 
-    const { log } = harContent
-    const { entries } = log
+    const segments = path.split('/').filter(Boolean)
 
-    const paths: Paths = {}
-    const servers: any = []
+    let tagIndex = segments.indexOf(mostCommonSegment) + 1
+    let tag = segments[tagIndex] || 'Miscellaneous'
 
-    for (const entry of entries) {
-        const { request, response } = entry
-        const url = new URL(request.url)
-        let path = url.pathname
-
-        if (!servers.find((s: any) => s.url === `${url.protocol}//${url.host}`)) {
-            servers.push({ url: `${url.protocol}//${url.host}` })
-        }
-
-        if (response.content.mimeType !== 'application/json') {
-            continue // Skip if not a JSON response.
-        }
-
-        if (!paths[path]) {
-            paths[path] = {} // Create path if it doesn't exist
-        }
-
-        const method = request.method.toLowerCase()
-
-        if (!paths[path][method]) {
-            paths[path][method] = {
-                parameters: [],
-                responses: {},
-                summary: url.href,
-            }
-        }
-
-        const queryParamPath = convertToQueryParams(path, paths[path][method].parameters) // Convert path segments to query parameters like /api/user/1234 to /api/user/:userId
-
-        if (queryParamPath !== path) {
-            paths[queryParamPath] = paths[path]
-            delete paths[path]
-            path = queryParamPath
-        }
-
-        // Query parameters
-
-        if (request.queryString) {
-            for (const q of request.queryString) {
-                if (!hasParameter(paths[path][method].parameters, q.name, 'query')) {
-                    console.log('path, method :>> ', path, method, q)
-                    paths[path][method].parameters.push({
-                        name: q.name,
-                        in: 'query',
-                        required: true,
-                        schema: { type: 'string', default: q.value }, // Add default value here
-                    })
-                }
-            }
-        }
-
-        // Headers
-        if (request.headers) {
-            for (const h of request.headers) {
-                if (
-                    h.name.toLowerCase() !== 'host' &&
-                    !hasParameter(paths[path][method].parameters, h.name, 'header')
-                ) {
-                    paths[path][method].parameters.push({
-                        name: h.name,
-                        in: 'header',
-                        required: true,
-                        schema: { type: 'string', default: h.value }, // Add default value here
-                    })
-                }
-            }
-        }
-
-        // Request Body (if it's JSON)
-        if (
-            request.postData &&
-            request.postData.mimeType === 'application/json' &&
-            request.postData.text
-        ) {
-            try {
-                const requestBody = JSON.parse(request.postData.text)
-                paths[path][method].requestBody = {
-                    content: {
-                        'application/json': {
-                            schema: generateJsonSchema(requestBody),
-                            example: requestBody, // Add sample request body as an example
-                        },
-                    },
-                }
-            } catch (err) {
-                console.error('Error parsing JSON request body from HAR:', err)
-            }
-        }
-
-        // Response
-
-        if (response.content.text) {
-            try {
-                const responseBody = JSON.parse(response.content.text)
-                const responseSchema = generateJsonSchema(responseBody)
-                paths[path][method].responses[response.status] = {
-                    description: 'Generated from HAR',
-                    content: {
-                        'application/json': {
-                            schema: responseSchema,
-                        },
-                    },
-                }
-            } catch (err) {
-                console.error('Error parsing JSON response body from HAR:', err)
-            }
-        } else {
-            paths[path][method].responses[response.status] = {
-                description: 'Generated from HAR',
-            }
-        }
-    }
-
-    assignTagsToPaths(paths)
-
-    // await validateSwagger(paths) // Remove for now.
-
-    const report = generateReport(paths)
-    logger.info(`API Report: ${JSON.stringify(report, null, 2)}`)
-
-    const swaggerDocs = {
-        openapi: '3.0.0',
-        info: {
-            title: 'Reverse Any Frontendw API with site-to-swagger - Example Docs',
-            version: '1.0.0',
-            description:
-                'Automatically generate OpenAPI (Swagger) documentation from HTTP Archive (HAR) files. This tool extracts endpoint data and responses from HAR files to produce comprehensive OpenAPI documentation for reverse hackery. It supports parsing of requests, responses, JSON body translations, and more.',
-            contact: {
-                name: 'Dougie Silkstone',
-                email: 'doug@withseismic.com',
-                url: 'https://www.withseismic.com',
-                'x-twitter': '@dougiesilkstone',
-            },
-            license: {
-                name: 'Custom License',
-                url: 'https://www.withseismic.com/license',
-            },
-        },
-        externalDocs: {
-            description: 'Github Example Link',
-            url: 'https://github.com/dougwithseismic/site-to-swagger',
-        },
-        servers: servers,
-
-        paths: paths,
-    }
-
-    return swaggerDocs
+    return [tag]
 }
 
 function getMostCommonSegment(paths: any): string {
@@ -270,23 +318,6 @@ function getMostCommonSegment(paths: any): string {
     return mostCommonSegment
 }
 
-function assignTagsToPaths(paths: any): void {
-    const mostCommonSegment = getMostCommonSegment(paths)
-
-    for (const path in paths) {
-        const segments = path.split('/').filter(Boolean)
-
-        let tagIndex = segments.indexOf(mostCommonSegment) + 1
-        let tag = segments[tagIndex] || 'Miscellaneous'
-
-        for (const method in paths[path]) {
-            if (!paths[path][method].tags) {
-                paths[path][method].tags = []
-            }
-            paths[path][method].tags.push(tag)
-        }
-    }
-}
 /**
  * Generates a JSON schema for the given JSON value.
  * @param jsonObj The JSON value to generate a schema for.
@@ -370,7 +401,11 @@ function generateObjectSchema(jsonObj: JsonObject): JsonSchema {
     }
 }
 
-function hasParameter(params: any[], name: string, location: string): boolean {
+function hasParameter(
+    params: { name: string; in: string }[],
+    name: string,
+    location: string
+): boolean {
     return params.some((param) => param.name === name && param.in === location)
 }
 
@@ -403,6 +438,35 @@ async function validateSwagger(swagger: any) {
         console.log('Swagger is valid!')
     } catch (err) {
         console.error('Invalid Swagger:', err)
+    }
+}
+
+function createSwaggerDocs(paths: Paths, servers: Array<{ url: string }>): object {
+    return {
+        openapi: '3.0.0',
+        info: {
+            title: 'Reverse Any Frontendw API with site-to-swagger - Example Docs',
+            version: '1.0.0',
+            description:
+                'Automatically generate OpenAPI (Swagger) documentation from HTTP Archive (HAR) files. This tool extracts endpoint data and responses from HAR files to produce comprehensive OpenAPI documentation for reverse hackery. It supports parsing of requests, responses, JSON body translations, and more.',
+            contact: {
+                name: 'Dougie Silkstone',
+                email: 'doug@withseismic.com',
+                url: 'https://www.withseismic.com',
+                'x-twitter': '@dougiesilkstone',
+            },
+            license: {
+                name: 'Custom License',
+                url: 'https://www.withseismic.com/license',
+            },
+        },
+        externalDocs: {
+            description: 'Github Example Link',
+            url: 'https://github.com/dougwithseismic/site-to-swagger',
+        },
+        servers: servers,
+
+        paths: paths,
     }
 }
 
